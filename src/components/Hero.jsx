3a1944031c05a58c7fd2e948391c817a0388518c
Wiktor Leszkowicz <w.leszkowicz99@gmail.com>
2024-03-29 14:18:24 +0100

    logo fix i hope

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,10 @@
-
-
+import logo from "./logo.svg"
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      Hero
+      <nav className='flex justify-between items-center flex-col'>
+        <img src={logo} alt="sumz_logo" className='w-27 object-cointain' />
+      </nav>
     </header>
   );
 };
