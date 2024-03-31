c6b7bc6bd13839c01b72f5b9a63c15049be1f7f2
FakeGods <w.leszkowicz99@gmail.com>
2024-03-31 23:57:39 +0200

    css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,16 @@
-import logo from "./logo.svg"
+import logo from "./logo.svg";
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      <nav className='flex justify-between items-center flex-col'>
-        <img src={logo} alt="sumz_logo" className='w-27 object-cointain' />
-      <button type="button" onClick={() => window.open("https://www.github.com/FakeGods")} className="black_btn">GitHub </button>
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className="w-27 object-cointain" />
+        <button
+          type="button"
+          onClick={() => window.open("https://www.github.com/FakeGods")}
+          className="black_btn"
+        >
+          GitHub{" "}
+        </button>
       </nav>
     </header>
   );
