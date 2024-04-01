f1494df246442000c41fc8705d20581d42152518
FakeGods <w.leszkowicz99@gmail.com>
2024-04-01 21:25:45 +0200

    1

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,8 +1,30 @@
 import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
   return (
-    <div className="w-full flex justify-center items-center flex-col">Demo</div>
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        <form
+          action=""
+          className="relative flex justify-center items-center"
+          onSubmit={() => {}}
+        ></form>
+        <img
+          src="{linkIcon}"
+          alt="linkicon"
+          className="absoulte left-0 my-2 ml-3 w-5"
+        />
+        <input
+          type="url"
+          placeholder="Enter a URL"
+          value=""
+          onChange={() => {}}
+          required
+          className="url_input peer"
+        />
+      </div>
+    </section>
   );
 };
 
