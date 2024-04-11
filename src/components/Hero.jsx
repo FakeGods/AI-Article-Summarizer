1c85deb6fb825acba25d62aefc450e379de695da
FakeGods <w.leszkowicz99@gmail.com>
2024-04-11 15:07:42 +0200

    OG

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-
 import logo from "../assets/logo.svg";
 import { Switch } from "@headlessui/react";
 
 const Hero = () => {
   const [enabled, setEnabled] = useState(false);
+
   return (
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center w-full mb-10 pt-3">
@@ -20,13 +20,7 @@ const Hero = () => {
           >
             GitHub
           </button>
-          <Switch
-            checked={enabled}
-            onChange={setEnabled}
-            className={`${
-              enabled ? "bg-gray-800 " : "bg-gray-200"
-            } relative inline-flex h-6 w-11 items-center rounded-full`}
-          >
+          <Switch>
             <span className="sr-only">Enable notifications</span>
             <span
               className={`${
@@ -42,8 +36,9 @@ const Hero = () => {
         <span className="orange_gradient ">OpenAI GPT-4</span>
       </h1>
       <h2 className="desc">
-        Simplify your reading with Summize, an open-source article summarizer
-        that transforms lengthy articles into clear and concise summaries
+        Make reading easier with Minimize, a free tool that shortens long
+        articles into brief summaries. It simplifies your reading experience by
+        condensing complex content into clear and concise summaries
       </h2>
     </header>
   );
